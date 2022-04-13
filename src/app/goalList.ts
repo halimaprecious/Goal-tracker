5b96db6895dd3993fcdf77ff0da5bb6fd83452f1
halima precious <halimaprecious3@gmail.com>
2022-04-14 02:11:15 +0300

    ft:add goal list file

---
+++ src/app/goalList.ts
@@ -0,0 +1,10 @@
+import { Goal } from "./goal";
+
+export let Goals:Goal[]=[
+    new Goal (1, 'Read Rich Dad Poor Dad','Get to understand Financial intelligence',new Date(2022,5,2)),
+    new Goal (2, 'Book a flight ticket','Flying to SA!!',new Date(2022,10,15)),
+    new Goal (3,'Buy new phone','The old one is cracked',new Date(2022,4,25)),
+    new Goal (4, 'Go hicking','To experience mother nature',new Date(2022,5,25)),
+    new Goal (5, 'Do more code practise','For mastery',new Date(2022,3,6)),
+    new Goal (6, 'Write a poem','Paper has more patience than people!!',new Date(2022,3,5)),
+];
