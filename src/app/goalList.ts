336102c995e7177747ddbb54ffb5e053d683a3f5
halima precious <halimaprecious3@gmail.com>
2022-04-14 19:58:05 +0300

    deleted one goal from the array

--- src/app/goalList.ts
+++ src/app/goalList.ts
@@ -5,6 +5,5 @@ export let Goals:Goal[]=[
     new Goal (2, 'Book a flight ticket','Flying to SA!!',new Date(2022,10,15)),
     new Goal (3,'Buy new phone','The old one is cracked',new Date(2022,4,25)),
     new Goal (4, 'Go hicking','To experience mother nature',new Date(2022,5,25)),
-    new Goal (5, 'Do more code practise','For mastery',new Date(2022,3,6)),
     new Goal (6, 'Write a poem','Paper has more patience than people!!',new Date(2022,3,5)),
 ];
